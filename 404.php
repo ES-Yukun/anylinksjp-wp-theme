8e30b51b2554ec54c58e1eff1d2fd7498e4fdb2d
SE-Yukun <yukun@team.anylinks.jp>
2022-12-09 17:24:10 +0000

    First Commit

---
+++ 404.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>anylinksjp 404 Not Found</title>
+    <style>
+        @font-face {
+            font-family: 'Open Sans';
+            src: url(./fonts/opensans/static/OpenSans/OpenSans-Medium.ttf);
+        }
+
+        * {
+            margin: 0;
+            padding: 0;
+            font-family: 'Open Sans';
+        }
+
+        body {
+            background-color: black;
+
+            background: linear-gradient(-45deg, black, #140a1f, #0c0c27) fixed;
+            background-size: 800% 800%;
+            animation: GradietionAnimation 9s ease infinite;
+        }
+
+        @keyframes GradietionAnimation {
+            0% {
+                background-position: 0% 0%
+            }
+
+            50% {
+                background-position: 50% 50%
+            }
+
+            100% {
+                background-position: 0% 0%
+            }
+        }
+
+        a {
+            text-decoration: none;
+            color: #0060BF;
+        }
+
+        a:hover {
+            text-decoration: underline;
+        }
+
+        #message {
+            left: 0;
+            right: 0;
+            top: 0;
+            bottom: 0;
+            margin-left: auto;
+            margin-right: auto;
+            margin-top: 35vh;
+            vertical-align: middle;
+            text-align: center;
+        }
+
+        #message h1 {
+            margin: 10px;
+            letter-spacing: 2px;
+        }
+    </style>
+    <script>
+        window.onload = async () => {
+            let linkHome = document.getElementById("link_home");
+            linkHome.setAttribute('href', window.location.origin);
+        }
+    </script>
+</head>
+
+<body>
+    <div id="message">
+        <h1>Sorry...</h1>
+        <p>We can't seem to find the page you're looking for.</p>
+        <p>You can go back to our <a id="link_home" href="">home</a>.</p>
+    </div>
+</body>
+
+</html>
---
+++ functions.php
@@ -0,0 +1,25 @@
+<?php
+function themeSetup()
+{
+    add_theme_support('post-thumbnails');       // Enable eye-catching image
+    add_theme_support('automatic-feed-links');  // Enable RSS feed for Posts and Comments
+    add_theme_support('title-tag');             // Enable automatic Title generation
+
+    add_theme_support('html5', array(           // Configure output via HTML
+        'search-form',
+        'comment-form',
+        'comment-list',
+        'gallery',
+        'caption',
+    ));
+}
+add_action('after_setup_theme', 'themeSetup');
+
+function scriptInit()
+{
+    wp_deregister_script('jquery');                                                                                     // Disable jQuery included in wordpress
+    wp_enqueue_script('jquery', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js', '3.6.1', 'true');  // Add latest jQuery from CDN
+    wp_enqueue_script('main-js', get_template_directory_uri().'/js/main.js', array('jquery'), '0.1.0', true);           // Add theme javascript file ( THEME_DIR/js/main.js )
+    wp_enqueue_style('default-css', get_template_directory_uri().'/css/default.css', array(), '0.1.0');                 // Add theme css file        ( THEME_DIR/css/default.js )
+}
+add_action('wp_enqueue_scripts', 'scriptInit');
