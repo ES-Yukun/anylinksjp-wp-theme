a58cb220e5f764f639daf264b6cab0435c2a34bc
SE-Yukun <yukun@team.anylinks.jp>
2022-12-09 18:30:45 +0000

    update

--- functions.php
+++ functions.php
@@ -18,9 +18,10 @@ add_action('after_setup_theme', 'themeSetup');
 function scriptInit()
 {
     wp_deregister_script('jquery');                                                                                     // Disable jQuery included in wordpress
-    
+
     wp_enqueue_script('jquery', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js', '3.6.1');          // Add latest jQuery from CDN
-    wp_enqueue_script('main-js', get_template_directory_uri().'/js/main.js', array('jquery'), '0.1.0');                 // Add theme javascript file ( THEME_DIR/js/main.js )
-    wp_enqueue_style('default-css', get_template_directory_uri().'/css/default.css', array(), '0.1.0');                 // Add theme css file        ( THEME_DIR/css/default.js )
+    wp_enqueue_script('js-main', get_template_directory_uri() . '/js/main.js', array('jquery'), '0.1.0');               // Add theme javascript file ( THEME_DIR/js/main.js )
+    wp_enqueue_style('css-init', get_template_directory_uri() . '/css/init.css', array(), '0.1.0');                     // Add theme css file        ( THEME_DIR/css/init.css )
+    wp_enqueue_style('css-default', get_template_directory_uri() . '/css/default.css', array('css-init'), '0.1.0');     // Add theme css file        ( THEME_DIR/css/default.css )
 }
 add_action('wp_enqueue_scripts', 'scriptInit');
---
+++ header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="anylinksjpによるクラウド・アプリケーションの開発支援. anylinks k8s platformでは、パートナー様やお客様にkubernetesを基盤とした独自アプリケーションの開発支援をしています.">
+    <title>anylinksjp</title>
+    <?php wp_head(); ?>
+</head>
+<body>
+    <header></header>
